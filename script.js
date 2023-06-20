44396636b4559d9181424e05bf3cee4b15245432
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-20 20:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+ const Formele=document.getElementById("form1");
+	const name=Formele.elements['fname'].value;
+	const lname=Formele.elements['lname'].value;
+	alert(`${name} ${lname}`);
 }
